e675905ad93f7f1f5762dc0070c55e4939da7f82
Don Collo <dmacollo@yahoo.com>
2017-02-04 01:36:53 -0800

    Added base application for devopsbuddy

---
+++ src/main/java/com/devopsbuddy/DevopsbuddytempApplication.java
@@ -0,0 +1,12 @@
+package com.devopsbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsbuddytempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsbuddytempApplication.class, args);
+	}
+}
