ccc0036f249bb37a369112602a3ccef26b1b3716
Don Collo <dmacollo@yahoo.com>
2017-02-04 15:08:46 -0800

    Added Index Controller and index.html placeholder

--- src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevopsbuddytempApplication {
+public class DevopsbuddyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevopsbuddytempApplication.class, args);
+		SpringApplication.run(DevopsbuddyApplication.class, args);
 	}
 }
--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DevopsbuddytempApplicationTests {
+public class DevopsbuddyApplicationTests {
 
 	@Test
 	public void contextLoads() {
