0a606c8da2be4d9768854e148d967e12d817ef38
Don Collo <dmacollo@yahoo.com>
2017-02-06 18:08:29 -0800

    Added about feature

---
+++ src/main/java/com/devopsbuddy/controllers/CopyController.java
@@ -0,0 +1,13 @@
+package com.devopsbuddy.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CopyController {
+	@RequestMapping(value="/about",method=RequestMethod.GET)
+	public String about(){
+		return "copy/about";
+	}
+}
