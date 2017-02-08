3c52ed5e3746a24a10e42d3734a2b8e146f90824
Don Collo <dmacollo@yahoo.com>
2017-02-07 17:50:46 -0800

    Created a payload page

---
+++ src/main/java/com/devopsbuddy/controllers/PayloadController.java
@@ -0,0 +1,15 @@
+package com.devopsbuddy.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PayloadController {
+	private static final String PAYLOAD_VIEW_NAME="payload/payload";
+	
+	@RequestMapping(value="/payload",method=RequestMethod.GET)
+	public String payload(){		
+		return PayloadController.PAYLOAD_VIEW_NAME;
+	}
+}
